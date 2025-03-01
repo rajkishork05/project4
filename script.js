7f4b5e590cbd5c6e16438fb19ec515bb62abeeb6
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-03-01 12:31:06 +0530

    server

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // i wanted to add new feature and making new branch
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
